6ad2aef8fc32915c0f118f04e694e4dd71c115d9
dariuszewski <dariuszewski32@gmail.com>
2022-10-08 17:07:16 +0200

    application setup

---
+++ flaskr/__init__.py
@@ -0,0 +1,37 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    # The instance folder is designed to not be under version control and be deployment specific. 
+    # It’s the perfect place to drop things that either change at runtime or configuration files.
+    # TODO: 'try to parametrize instance_relative_config variable to create staging and production environments'
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite') # this is only if used sqlite
+    )
+
+    if test_config is None:  # this will override app.config.from_mapping()
+        # load the instance config if not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load testing config when passed in
+        app.config.from_mapping(test_config) # special configuration for testing purposes
+
+    # ensure the instance folder exists
+    # this is only for sqlite
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # hello world page
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
