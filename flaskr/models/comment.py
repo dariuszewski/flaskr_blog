6510882dc8c5dbd88a0b14dae03a40913d769cec
dariuszewski <dariuszewski32@gmail.com>
2022-11-08 20:46:31 +0100

    add comment model

---
+++ flaskr/models/comment.py
@@ -0,0 +1,29 @@
+from os import stat
+from sqlalchemy import desc
+
+from flaskr.extensions import db
+
+
+# SQLAlchemy automatically defines an __init__ method for each model that assigns 
+# any keyword arguments to corresponding database columns and other attributes.
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    user = db.relationship('User', back_populates="comments", passive_deletes=True)
+    post = db.relationship('Post', back_populates="comments", passive_deletes=True)
+
+    @staticmethod
+    def get_comments_by_post(post_id):
+        return db.session.execute(db
+            .select(Comment)
+            .filter_by(post_id=post_id)).all()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
