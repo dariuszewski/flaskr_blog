f23e3a3d5b64644ed2b7999f0cf4d211a6fb857b
dariuszewski <dariuszewski32@gmail.com>
2023-02-05 21:07:48 +0100

    flash message

--- flaskr/blueprints/blog/index.py
+++ flaskr/blueprints/blog/index.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request
+    Blueprint, render_template, request, flash
 )
 
 from flaskr.models.post import Post
@@ -13,11 +13,16 @@ bp = Blueprint('index', __name__)
 def index():
     if request.method == 'POST':
         keyword = request.form['keyword']
-        posts = Post.get_posts_by_phrase(keyword)
-        return render_template('blog/index.html', posts=posts)
-
+        posts = Post.get_posts_by_phrase(keyword).all()
+        if posts:
+            return render_template('blog/index.html', posts=posts, keyword=keyword)
+        else:
+            message = f"Can't find posts matching criterium: '{keyword}'"
+            flash(message)
+            
     posts = Post.get_all()
     if request.args:
+        print('AM I HERE???')
         searched_tag = request.args['tag']
         filtered_posts = []
         for post in posts:
@@ -25,6 +30,6 @@ def index():
                 if searched_tag == post_tag.body:
                     filtered_posts.append(post)
         posts = list(set(filtered_posts))
-        return render_template('blog/index.html', posts=posts, searched_tag=searched_tag)
+        return render_template('blog/index.html', posts=posts, keyword=searched_tag)
 
-    return render_template('blog/index.html', posts=posts)
+    return render_template('blog/index.html', posts=posts, keyword=None)
